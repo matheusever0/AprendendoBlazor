e44ccadaf05412722b9c9f8c5496e634125ea2b9
Matheus Severo <matheus.severo@tcm.ba.gov.br>
2023-01-03 10:45:18 -0300

    Retornar valores de inputs

---
+++ Blazor4/Model/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor4.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
