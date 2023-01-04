ebe586e36670f68829abad67451d5c4248262772
Matheus Severo <matheus.severo@tcm.ba.gov.br>
2023-01-04 11:33:41 -0300

    CEP e Countries

---
+++ Blazor4/Model/CEP.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+#nullable disable
+namespace Blazor4.Model
+{
+    public class CEP
+    {
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("city")]
+        public string City { get; set; }
+
+        [JsonPropertyName("district")]
+        public string District { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("ok")]
+        public bool Ok { get; set; }
+
+        [JsonPropertyName("statusText")]
+        public string StatusText { get; set; }
+
+
+    }
+}
